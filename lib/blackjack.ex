7901b9fc49bae4357824adc088c1e3b64ac097bc
Pat Maddox <pat@geometer.io>
2021-01-21 13:55:47 -0800

    Extract a file

--- lib/blackjack.ex
+++ lib/blackjack.ex
@@ -1,33 +1,15 @@
 defmodule Blackjack do
-  defmodule Player do
-    defstruct ~w[name cash chips]a
-  end
+  alias Blackjack.Player
 
   def new_player(name, cash) do
-    %Player{name: name, cash: cash, chips: 0}
+    Player.new(name, cash)
   end
 
   def buy_chips(player, amount) do
-    player
-    |> increase(:chips, amount)
-    |> decrease(:cash, amount)
+    Player.buy_chips(player, amount)
   end
 
   def cash_out(player, amount) do
-    player
-    |> increase(:cash, amount)
-    |> decrease(:chips, amount)
-  end
-
-  defp increase(player, money_bag, amount) do
-    Map.update!(player, money_bag, fn current_value -> current_value + amount end)
-  end
-
-  defp decrease(player, money_bag, amount) do
-    if amount > Map.get(player, money_bag) do
-      {:error, "Not enough #{money_bag}"}
-    else
-      Map.update!(player, money_bag, fn current_value -> current_value - amount end)
-    end
+    Player.cash_out(player, amount)
   end
 end
---
+++ lib/blackjack/player.ex
@@ -0,0 +1,31 @@
+defmodule Blackjack.Player do
+  defstruct ~w[name cash chips]a
+
+  def new(name, cash) do
+    %__MODULE__{name: name, cash: cash, chips: 0}
+  end
+
+  def buy_chips(player, amount) do
+    player
+    |> increase(:chips, amount)
+    |> decrease(:cash, amount)
+  end
+
+  def cash_out(player, amount) do
+    player
+    |> increase(:cash, amount)
+    |> decrease(:chips, amount)
+  end
+
+  defp increase(player, money_bag, amount) do
+    Map.update!(player, money_bag, fn current_value -> current_value + amount end)
+  end
+
+  defp decrease(player, money_bag, amount) do
+    if amount > Map.get(player, money_bag) do
+      {:error, "Not enough #{money_bag}"}
+    else
+      Map.update!(player, money_bag, fn current_value -> current_value - amount end)
+    end
+  end
+end
